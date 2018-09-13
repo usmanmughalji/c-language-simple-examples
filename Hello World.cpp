e691353f77b8ac8d29b42c4a5eeef0bc49e201c7
usmanmughalji <35332218+usmanmughalji@users.noreply.github.com>
2018-09-13 19:07:24 +0500

    Create Hello World.cpp

---
+++ Hello World.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+
+{
+	cout<<"Hello World!"<<endl;
+	cout<<endl;
+	cout<<"Just Simple Text Display";
+	getch();
+	return 0;
+}
